039aa6f81655e8ece3557a8f12a8712771e1ea78
abhishekgarfield <abhishek2759@gmail.com>
2024-03-16 20:46:50 +0530

    study logs 16/03/2024

---
+++ swift/swiftui_docs/Landmarks/Landmarks/Modal/LandMarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandMarkRow.swift
+//  Landmarks
+//
+//  Created by Abhishek Sharma on 16/03/24.
+//
+
+import SwiftUI
+
+struct LandMarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandMarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandMarkRow()
+    }
+}
---
+++ swift/swiftui_docs/Landmarks/Landmarks/Views/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Abhishek Sharma on 13/03/24.
+//
+//
+//import SwiftUI
+//import MapKit
+//import CoreLocation;
+//
+//struct MapView: View {
+//    private var region: MKCoordinateRegion {
+//        MKCoordinateRegion(
+//            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+//            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+//        )
+//    }
+//    var body: some View {
+//        Map(coordinateRegion:region)
+//    }
+//
+//
+//    
+//}
+//
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView()
+//    }
+//}
